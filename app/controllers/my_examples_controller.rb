eed3647e575acd00fc1f355fc36e244221dd7f55
Alcy Torres <mxalcytorres@gmail.com>
2024-05-12 21:45:06 -0400

    finish random fortune

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,5 +1,6 @@
 class MyExamplesController < ApplicationController
   def fortune
-    render json: {message: "Hello this is a test."}
+    fortunes = ["Good day", "Happy day", "Good health"]
+    render json: {your_fortune: fortunes.sample}
   end
 end
